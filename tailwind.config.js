2b806d3c68d45b169a304847e432e42a10af2a9c
afifudinn <work.afifudin@gmail.com>
2022-02-09 21:46:53 +0700

    adding Inter and MPlus Rounded Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
   mode: "jit",
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        mplus: ["MPlus", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
