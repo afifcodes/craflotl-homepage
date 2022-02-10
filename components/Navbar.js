3fc6e3ab064da4e4ee3e58cd07bad81bd01c5d8e
afifudinn <work.afifudin@gmail.com>
2022-02-10 11:56:32 +0700

    update AnimatePresence on Page Transition and Theme Changing

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { Menu, Moon, Sun } from "react-feather";
 import useTheme from "../lib/useTheme";
@@ -8,9 +9,11 @@ export default function Navbar() {
   return (
     <div className="fixed top-0 w-full bg-opacity-50 border-b dark:border-b-neutral-800 backdrop-blur-md">
       <div className="max-w-3xl m-auto flex items-center justify-between px-8 py-2">
-        <p className="font-eudoxus font-bold text-lg tracking-tight">
-          Afifudin Abdullah
-        </p>
+        <Link href="/">
+          <a className="font-eudoxus font-bold text-lg tracking-tight">
+            Afifudin Abdullah
+          </a>
+        </Link>
         <div className="flex items-center justify-end gap-4 text-sm font-medium">
           <Link href="/works" scroll={false}>
             <a className="px-2 py-1 rounded bg-blue-100 dark:bg-amber-100 text-blue-600 dark:text-[#202023]">
@@ -27,16 +30,44 @@ export default function Navbar() {
               Source
             </a>
           </Link>
-          <button
-            onClick={toggleDarkMode}
-            className="hidden sm:flex rounded-md w-10 h-10 outline-none items-center justify-center bg-violet-600 dark:bg-amber-100"
-          >
-            {theme === "light" ? (
-              <Moon className="h-5 w-5 text-neutral-200" />
-            ) : (
-              <Sun className="h-5 w-5 text-[#202023]" />
-            )}
-          </button>
+          <div className="relative h-10">
+            <AnimatePresence>
+              {theme === "light" && (
+                <motion.button
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={{
+                    initial: { opacity: 0, y: -75 },
+                    animate: { opacity: 1, y: 0 },
+                    exit: { opacity: 0, y: 75 },
+                  }}
+                  onClick={toggleDarkMode}
+                  className="absolute hidden sm:flex rounded-md w-10 h-10 outline-none items-center justify-center bg-violet-600"
+                >
+                  <Moon className="h-5 w-5 text-neutral-200" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+            <AnimatePresence>
+              {theme === "dark" && (
+                <motion.button
+                  initial="initial"
+                  animate="animate"
+                  exit="exit"
+                  variants={{
+                    initial: { opacity: 0, y: -75 },
+                    animate: { opacity: 1, y: 0 },
+                    exit: { opacity: 0, y: 75 },
+                  }}
+                  onClick={toggleDarkMode}
+                  className="absolute hidden sm:flex rounded-md w-10 h-10 outline-none items-center justify-center bg-amber-100"
+                >
+                  <Sun className="h-5 w-5 text-[#202023]" />
+                </motion.button>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
         <button className="sm:hidden rounded-md w-10 h-10 outline-none flex items-center justify-center bg-white">
           <Menu className="h-5 w-5 text-[#202023]" />
--- pages/_app.js
+++ pages/_app.js
@@ -9,13 +9,17 @@ function MyApp({ Component, pageProps, router }) {
     <GlobalProvider>
       <Layout>
         <Navbar />
-        <AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
           <motion.div
             key={router.route}
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-            exit={{ opacity: 0, y: 50 }}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={{
+              initial: { opacity: 0, y: 50 },
+              animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              exit: { opacity: 0, y: 50, transition: { duration: 0.5 } },
+            }}
           >
             <Component {...pageProps} />
           </motion.div>
